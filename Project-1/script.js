deed3505f45a07be07a24fd7c5f9e3f9009c88c1
PriyanshuRawat24 <rawatpriyanshu653@gmail.com>
2024-10-07 16:29:08 +0530

    update

--- Project-1/script.js
+++ Project-1/script.js
@@ -16,33 +16,3 @@ $('.owl-carousel').owlCarousel({
       }
   }
 });
-
-
-// counter run only once when visible in screen
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const counter = entry.target;
-        let endValue = counter.textContent;
-        let startValue = 0;
-        let updating = setInterval(() => {
-          startValue += endValue / 200;
-          counter.textContent = startValue.toFixed(0);
-          if (startValue > endValue) {
-            counter.textContent = endValue;
-            clearInterval(updating);
-            observer.unobserve(counter);
-          }
-        }, 10);
-      }
-    });
-  },
-  { threshold: 1 }
-);
-document
-  .querySelectorAll(".counter")
-  .forEach((counter) => observer.observe(counter));
-
-
